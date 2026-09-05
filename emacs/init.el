9c104a31b8e9278f972a4012e4a8588152ece217
tohenton <tohenton@gmail.com>
2026-09-05 21:35:15 +0900

    emacs: Recover corfu-terminal setting with condition

--- emacs/init.el
+++ emacs/init.el
@@ -188,6 +188,17 @@
       :config
       (add-to-list 'corfu-margin-formatters #'kind-icon-margin-formatter))
     )
+  (leaf corfu-terminal
+    :url "https://codeberg.org/akib/emacs-corfu-terminal.git"
+    :when (and (< emacs-major-version 31)
+               (eq (framep-on-display) 't))
+    :el-get (emacs-corfu-terminal :url "https://codeberg.org/akib/emacs-corfu-terminal.git")
+    :config
+    (leaf popon
+      :url "https://codeberg.org/akib/emacs-popon.git"
+      :unless (display-graphic-p)
+      :el-get (emacs-popon :url "https://codeberg.org/akib/emacs-popon.git"))
+    (corfu-terminal-mode 1))
   (leaf cape
     :url "https://github.com/minad/cape"
     :if (display-graphic-p)
