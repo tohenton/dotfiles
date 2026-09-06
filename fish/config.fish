a04cfc24cf85a3c2e95d42fa0641560ee9da5f76
tohenton <tohenton@gmail.com>
2026-09-06 20:08:19 +0900

    fish: Change brew running platform macOS only

--- fish/config.fish
+++ fish/config.fish
@@ -2,11 +2,10 @@
 # brew
 ################################################################################
 if status is-interactive
-    if test -e /opt/homebrew/bin/brew
-        eval (/opt/homebrew/bin/brew shellenv)
-    end
-    if test -e /home/linuxbrew/.linuxbrew/bin/brew
-        eval (/home/linuxbrew/.linuxbrew/bin/brew shellenv)
+    if test (uname) = Darwin
+        if test -e /opt/homebrew/bin/brew
+            eval (/opt/homebrew/bin/brew shellenv)
+        end
     end
 end
 
